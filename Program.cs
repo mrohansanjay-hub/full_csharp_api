6c52ad63713eb851d6278189de19acd88942bdf6
mrohansanjay-hub <mrohansanjay@gmail.com>
2025-11-27 20:06:46 +0530

    Implement CORS policy to allow all requests
    Added CORS support to allow all origins, methods, and headers.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using MyApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Controllers
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,11 +13,27 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()  // Allow all frontend URLs
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
+// Enable Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Use CORS
+app.UseCors("AllowAll");
+
+// Map API Controllers
 app.MapControllers();
 
 app.Run();
